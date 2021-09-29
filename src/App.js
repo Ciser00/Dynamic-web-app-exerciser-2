94ae77700ec8f7b2a43ed5c46dd82750b8493ca3
Ciser00 <47250680+Ciser00@users.noreply.github.com>
2021-09-29 19:45:42 -0400

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+//Main app file
+
 import './App.css';
+import Home from `./containers/Home`;
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path = `/`>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from `react`;
+
+function home(){
+  return (
+    <main className="App">
+      <header> Weather App </header>
+      <section> Weather data </section>
+      </main>
+  );
+}
+
+export default Home;
